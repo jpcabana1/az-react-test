02dd8779283159c994a43138a24d90eb111c32d7
João Pedro <jpcabana1@gmail.com>
2022-09-16 16:53:50 -0300

    api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
 
 function App() {
+  const [mensagem, setMensagem] = React.useState("Carregando...")
+
+  const hello = () =>
+    fetch('https://az-test-dotnet.azurewebsites.net/hello')
+      .then((res) => res.text())
+      .then((resp) => {
+        setMensagem(resp)
+        console.log(mensagem)
+      })
+      .catch((err) => {
+        setMensagem('Erro: ' + err.message)
+        console.log(mensagem)
+      })
+  React.useEffect(() => {
+    hello()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +27,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Azure API: {mensagem}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +38,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
